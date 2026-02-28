7a97cac13e7c98d1be6fa49c74779f2db77f7e33
MD SHAHADAT HOSSAIN <m.shahadat.hossain.it@gmail.com>
2026-02-28 23:15:12 +0600

    Update wsgi.py

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,24 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
-from django.core.management import call_command
-from django.contrib.auth import get_user_model
-from django.db import connection
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
 application = get_wsgi_application()
-
-try:
-    print("Running migrations...")
-    call_command('migrate', interactive=False)
-    
-    User = get_user_model()
-    username = "admin"
-    email = "admin@example.com"
-    password = "admin123"
-
-    if "api_user" in connection.introspection.table_names():
-        if not User.objects.filter(username=username).exists():
-            User.objects.create_superuser(username=username, email=email, password=password)
-            print(f"Superuser '{username}' created successfully!")
-except Exception as e:
-    print(f"Startup task error: {e}")
