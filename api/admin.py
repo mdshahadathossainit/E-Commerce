bd1b4cc8870397fbb60d919fed2062727f18f3c0
MD SHAHADAT HOSSAIN <m.shahadat.hossain.it@gmail.com>
2026-02-28 20:30:35 +0600

    admin.py

--- api/admin.py
+++ api/admin.py
@@ -1,1 +1,9 @@
 
+from django.contrib import admin
+from .models import User, Category, Product, Order, OrderItem
+
+admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderItem)
