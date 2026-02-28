22d3ff85e0c179ccb0aa0d5d27b5f82dc84dbd6f
MD SHAHADAT HOSSAIN <m.shahadat.hossain.it@gmail.com>
2026-02-28 20:44:38 +0600

    urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,24 @@
 
 from django.urls import path
-from .views import ProductListView, ProductDetailView, CategoryListView, OrderCreateView
+from .views import ProductListView, ProductDetailView, CategoryListView, OrderCreateView, RegisterView
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
+    path('products/', ProductListView.as_view()),
+    path('products/<slug:slug>/', ProductDetailView.as_view()),
+    path('categories/', CategoryListView.as_view()),
+    path('orders/', OrderCreateView.as_view()),
+]
+
+urlpatterns = [
+    path('register/', RegisterView.as_view()),
+    path('login/', TokenObtainPairView.as_view()),
+    path('token/refresh/', TokenRefreshView.as_view()),
+    
     path('products/', ProductListView.as_view()),
     path('products/<slug:slug>/', ProductDetailView.as_view()),
     path('categories/', CategoryListView.as_view()),
