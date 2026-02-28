d12cae87f425d405d41c69db9f0a88a5676a6b4d
MD SHAHADAT HOSSAIN <m.shahadat.hossain.it@gmail.com>
2026-02-28 20:33:49 +0600

    urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,1 +1,10 @@
 
+from django.urls import path
+from .views import ProductListView, ProductDetailView, CategoryListView, OrderCreateView
+
+urlpatterns = [
+    path('products/', ProductListView.as_view()),
+    path('products/<slug:slug>/', ProductDetailView.as_view()),
+    path('categories/', CategoryListView.as_view()),
+    path('orders/', OrderCreateView.as_view()),
+]
