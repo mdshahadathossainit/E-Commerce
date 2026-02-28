0ca568e915152bcf8948ea946468c91ebeb70eaa
MD SHAHADAT HOSSAIN <m.shahadat.hossain.it@gmail.com>
2026-02-28 23:14:56 +0600

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -60,7 +60,8 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         default='sqlite:///db.sqlite3',
-        conn_max_age=600
+        conn_max_age=0,
+        conn_health_checks=True,
     )
 }
 
