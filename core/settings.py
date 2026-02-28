46cdfe44913cf0e93e7361a4bbfa968900d9f85b
MD SHAHADAT HOSSAIN <m.shahadat.hossain.it@gmail.com>
2026-02-28 20:38:27 +0600

    settings.py

--- core/settings.py
+++ core/settings.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -28,3 +29,21 @@ MIDDLEWARE = [
 CORS_ALLOW_ALL_ORIGINS = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ],
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'AUTH_HEADER_TYPES': ('Bearer',),
+}
