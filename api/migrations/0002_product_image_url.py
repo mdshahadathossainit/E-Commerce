f7c7f6d7d3fc0f5d172d5b94c9514e8e836219aa
MD SHAHADAT HOSSAIN <m.shahadat.hossain.it@gmail.com>
2026-03-02 00:03:19 +0600

    Update 0002_product_image_url.py

--- api/migrations/0002_product_image_url.py
+++ api/migrations/0002_product_image_url.py
@@ -3,7 +3,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('api', '0001_initial.py'),
+        ('api', '0001_initial'), 
     ]
 
     operations = [
