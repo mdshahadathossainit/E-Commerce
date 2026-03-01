0b043a380cddcacea48cf47264e6632f7b3a04d9
MD SHAHADAT HOSSAIN <m.shahadat.hossain.it@gmail.com>
2026-03-01 21:44:48 +0600

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,3 @@
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -8,6 +7,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
