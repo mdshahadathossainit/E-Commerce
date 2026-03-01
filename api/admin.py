c6c79b18115a74341114a4679caf18488e09317c
MD SHAHADAT HOSSAIN <m.shahadat.hossain.it@gmail.com>
2026-03-01 21:34:27 +0600

    Update admin.py

--- api/admin.py
+++ api/admin.py
@@ -1,9 +1,18 @@
-
 from django.contrib import admin
 from .models import User, Category, Product, Order, OrderItem
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+    readonly_fields = ('product', 'quantity', 'price')
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'total_price', 'payment_method', 'is_paid', 'is_delivered', 'created_at')
+    list_filter = ('is_paid', 'is_delivered', 'payment_method')
+    search_fields = ('user__username', 'id')
+    inlines = [OrderItemInline]
 
 admin.site.register(User)
 admin.site.register(Category)
 admin.site.register(Product)
-admin.site.register(Order)
-admin.site.register(OrderItem)
