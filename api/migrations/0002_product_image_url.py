cb5aece76d424eda3395053f88d019fc9ed0b46e
MD SHAHADAT HOSSAIN <m.shahadat.hossain.it@gmail.com>
2026-03-02 00:02:04 +0600

    Create 0002_product_image_url.py

---
+++ api/migrations/0002_product_image_url.py
@@ -0,0 +1,20 @@
+from django.db import migrations, models
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('api', '0001_initial.py'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='product',
+            name='image_url',
+            field=models.URLField(blank=True, max_length=1000, null=True),
+        ),
+        migrations.AddField(
+            model_name='category',
+            name='image_url',
+            field=models.URLField(blank=True, max_length=500, null=True),
+        ),
+    ]
