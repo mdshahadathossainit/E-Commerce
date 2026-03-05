567d71a4afc28ae678229abb15b3b3b710b65d6d
MD SHAHADAT HOSSAIN <m.shahadat.hossain.it@gmail.com>
2026-03-05 11:53:51 +0600

    Update wsgi.py

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,23 +1,4 @@
 import os
-import time
 from django.core.wsgi import get_wsgi_application
-from django.core.management import call_command
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 application = get_wsgi_application()
-
-def run_startup_tasks():
-    try:
-        time.sleep(5) 
-        
-        print("Running database migrations...")
-        call_command('migrate', interactive=False)
-        from django.contrib.auth import get_user_model
-        User = get_user_model()
-        if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser("admin", "admin@example.com", "admin123")
-            print("Admin created successfully!")
-            
-    except Exception as e:
-        print(f"Startup task failed: {e}")
-run_startup_tasks()
