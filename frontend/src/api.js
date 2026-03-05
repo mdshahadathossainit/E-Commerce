87fcf0187ba0f6b8b5da2f5d6d47ee059d180ed2
MD SHAHADAT HOSSAIN <m.shahadat.hossain.it@gmail.com>
2026-03-05 12:57:18 +0600

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'https://e-commerce-hmvn.onrender.com/api/'
+    baseURL: 'https://e-commerce-hmvn.onrender.com/api/' 
 });
 
 API.interceptors.request.use((config) => {
